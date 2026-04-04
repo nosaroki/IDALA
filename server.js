bdf2dd81cb32aedc346b436f9a76a3930d80090c
nosaroki <nnena.osarokio@hotmail.fr>
2026-04-04 14:55:39 +0200

    ajout server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const app = express()
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(3000)
